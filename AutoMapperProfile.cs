080a9991380ae77c3704bc540b2c1bf8dc4f5797
Rolando Estrada <rolando836@revature.net>
2023-02-12 00:40:20 -0500

    Added a Update & Delete Controller

--- AutoMapperProfile.cs
+++ AutoMapperProfile.cs
@@ -11,6 +11,7 @@ namespace RPG_Roster
         {
             CreateMap<Character, GetCharacterDto>();
             CreateMap<AddCharacterDto, Character>();
+            CreateMap<UpdateCharacterDto, Character>();
         }
     }
 }
